0d97aa7cb786a54091fc5b2f823adf74108f117b
LGSF-DEV <lucasfonseca210@gmail.com>
2023-04-01 13:08:57 -0300

    ex5 atualização

---
+++ Bootcamp Orange Tech BackEnd/Trilha Java Básico/ARRAYS-E-LOOPS/Loops/src/Ex5_Tabuada.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex5_Tabuada {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o número da tabuada: ");
+        int tabuada = sc.nextInt();
+
+        System.out.print("Digite o multiplicador: ");
+        int multiplicador = sc.nextInt();
+        
+        System.out.printf("Tabuada de %d:%n ",tabuada);
+
+        for(int i = 1; i <= multiplicador; i++){
+            int resultado = tabuada * i;
+            System.out.println();
+            System.out.printf("%d X %d = %d",tabuada, i, resultado);
+        }
+        sc.close();
+        System.out.println();
+        System.out.print("Fim do programa!");
+    }
+    
+}
