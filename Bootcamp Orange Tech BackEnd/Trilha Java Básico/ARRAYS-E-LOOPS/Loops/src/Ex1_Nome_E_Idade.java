3a3789a626ed18756096b134387cb15ae7ef6d9b
LGSF-DEV <lucasfonseca210@gmail.com>
2023-04-01 11:04:06 -0300

    ex1

---
+++ Bootcamp Orange Tech BackEnd/Trilha Java Básico/ARRAYS-E-LOOPS/Loops/src/Ex1_Nome_E_Idade.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ex1_Nome_E_Idade {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while(true){
+            System.out.print("Digite o nome: ");
+            nome = sc.next();
+            if(nome.equals("0")) break;
+
+            System.out.print("Digite a idade: ");
+            idade = sc.nextInt();
+            
+        }
+
+        System.out.println("programa encerrado!");
+        sc.close();
+    }
+}
