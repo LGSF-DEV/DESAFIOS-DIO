7cc07b4bdc0239891d8b89265931d2cffbb682a2
LGSF-DEV <lucasfonseca210@gmail.com>
2023-04-01 14:43:34 -0300

    ex1

--- Bootcamp Orange Tech BackEnd/Trilha Java Básico/ARRAYS-E-LOOPS/Arrays/src/EX1_Ordem_Inversa.java
+++ Bootcamp Orange Tech BackEnd/Trilha Java Básico/ARRAYS-E-LOOPS/Arrays/src/EX1_Ordem_Inversa.java
@@ -3,10 +3,16 @@ public class EX1_Ordem_Inversa {
         
         int[] vetor = {-5, 10, -15, 20, -25, 30};
 
+        System.out.print("Vetor: ");
         int count = 0;
-        while (count < (vetor.length-1)) {
-            System.out.println(vetor[count]);
+        while (count < (vetor.length)) {
+            System.out.print(vetor[count] + " ");
             count++;
+        }
+        System.out.println();
+        System.out.print("Vetor: ");
+        for (int i = (vetor.length - 1); i >= 0; i --){
+            System.out.print(vetor[i] + " ");
         }
     }
 }
