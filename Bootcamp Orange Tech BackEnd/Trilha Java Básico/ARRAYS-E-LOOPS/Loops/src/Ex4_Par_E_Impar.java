13d51338092b390ef51b712d1b23f94af3675de1
LGSF-DEV <lucasfonseca210@gmail.com>
2023-04-01 12:40:36 -0300

    ex4

--- Bootcamp Orange Tech BackEnd/Trilha Java Básico/ARRAYS-E-LOOPS/Loops/src/Ex4_Par_E_Impar.java
+++ Bootcamp Orange Tech BackEnd/Trilha Java Básico/ARRAYS-E-LOOPS/Loops/src/Ex4_Par_E_Impar.java
@@ -4,9 +4,27 @@ public class Ex4_Par_E_Impar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int quant_numeros;
+        int numero;
+        int quant_pares = 0; 
+        int quant_impares = 0;
 
-
-
-        
+        System.out.print("Quantidade de números: ");
+        quant_numeros = sc.nextInt();
+        int count = 0;
+        do {
+            System.out.print("Número: ");
+            numero = sc.nextInt();
+            if(numero % 2 == 0){
+                quant_pares += 1;
+            }
+            else{
+                quant_impares += 1;
+            }
+            count += 1;
+        } while (count < quant_numeros);
+        sc.close();
+        System.out.printf("Quantidade de números pares: %d%n", quant_pares);
+        System.out.printf("Quantidade de números impares %d%n",quant_impares);
     }
 }
