abbefa200186944f574aba94871b79dcaff5ced7
LGSF-DEV <lucasfonseca210@gmail.com>
2023-04-02 17:22:04 -0300

    atualização

--- Bootcamp Orange Tech BackEnd/Trilha Java Básico/SOBRECARGA-JAVA/src/Quadrilatero.java
+++ Bootcamp Orange Tech BackEnd/Trilha Java Básico/SOBRECARGA-JAVA/src/Quadrilatero.java
@@ -15,14 +15,4 @@ public class Quadrilatero {
         return ((baseMaior+baseMenor)*altura) / 2;
     }
 
-    public static void xpto() {
-
-        System.out.println("Antes");
-        return;
-    }
-
-    public static double abc() {
-        return 1.6;
-    }
-
 }
